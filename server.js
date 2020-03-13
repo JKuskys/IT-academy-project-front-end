76fd84785453b41b53c3cf56aac8cdf9b5083851
Nedas Jankauskas <nedasj44@gmail.com>
2020-03-14 00:42:44 +0200

    heroku

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/IT-academy-project-front-end'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/IT-academy-project-front-end/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
-});
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
