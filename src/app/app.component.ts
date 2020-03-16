b89df72ed531bb349137bfcf81ee4bc2e0b0a3e1
jkuskys <j.kuskys1@gmail.com>
2020-03-16 18:38:41 +0200

    heroku fix?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'IT-academy-project-front-end';
+  title = "IT-academy-project-front-end";
 }
