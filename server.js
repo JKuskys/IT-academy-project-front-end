59d335c09fd304daeac1e08280ea8e7c2650007c
jkuskys <j.kuskys1@gmail.com>
2020-03-16 16:37:32 +0200

    registration post working

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 const app = express();
+const API_HOST = process.env.API_HOST || 'localhost:8080';
+const PORT = process.env.PORT || 3000;
+
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/IT-academy-project-front-end'));
+app.use('/api', proxy({
+  target: API_HOST,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': ''
+  }
+}));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/IT-academy-project-front-end/index.html'));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+app.listen(PORT);
