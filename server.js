ca57c8dafdf0337048f11046b83c915ef5be61ba
jkuskys <j.kuskys1@gmail.com>
2020-03-16 19:02:54 +0200

    heroku fix 3

--- server.js
+++ server.js
@@ -2,20 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-
 // Serve static files....
-app.use('/api', proxy({
-  target: API_HOST,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': ''
-  }
-}));
+app.use(express.static(__dirname + '/dist/IT-academy-project-front-end'));
 
 // Send all requests to index.html
-app.get('/*', function (req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/IT-academy-project-front-end/index.html'));
 });
 
 // default Heroku PORT
-app.listen(PORT);
+app.listen(process.env.PORT || 3000);
