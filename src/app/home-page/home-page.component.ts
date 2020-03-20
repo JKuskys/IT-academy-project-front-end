5831599dc77c0e49cf39b367f4bae69622f7ae16
JKuskys <48916581+JKuskys@users.noreply.github.com>
2020-03-20 17:38:11 +0200

    It5.2 (#24)
    * blocks are created
    
    * routes added
    
    * error fix

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ModalService} from '../Services/modal/modal.service';
-import {MatDialog} from '@angular/material/dialog'
-import { RegistrationComponent } from '../registration/registration.component';
+import {MatDialog} from '@angular/material/dialog';
+import {RegistrationComponent} from '../registration/registration.component';
+import {Application} from '../shared/application';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -9,14 +11,47 @@ import { RegistrationComponent } from '../registration/registration.component';
 })
 export class HomePageComponent implements OnInit {
   private bodyText: string;
+  //data for testing
+  applications: Application[];
 
-  constructor(private modalService: ModalService, private dialog: MatDialog) { }
+  constructor(private modalService: ModalService, private dialog: MatDialog) {
+    this.applications = [{
+      name: 'Testas Testauskas',
+      application_date: '2020-09-01',
+      comment_count: '0',
+      seen: false,
+      status: '',
+    },
+      {
+        name: 'Testas Testauskas',
+        application_date: '2020-09-01',
+        comment_count: '10',
+        seen: true,
+        status: 'accepted',
+      },
+      {
+        name: 'Testas Testauskas',
+        application_date: '2020-09-01',
+        comment_count: '3',
+        seen: true,
+        status: 'declined',
+      },
+      {
+        name: 'Testas Testauskas',
+        application_date: '2020-09-01',
+        comment_count: '0',
+        seen: false,
+        status: 'possible',
+      },
+    ];
+
+  }
 
   ngOnInit(): void {
     this.bodyText = 'This text can be updated in modal 1';
   }
 
-  openDialog(){
+  openDialog() {
     this.dialog.open(RegistrationComponent);
   }
 }
