97fd2ca9803ec979cbcead372979d246530cb0aa
mildapetr <58117403+mildapetr@users.noreply.github.com>
2020-03-24 13:40:21 +0200

    fixing CORS issues and getting application data from backend (#39)

--- src/app/Services/application.service.ts
+++ src/app/Services/application.service.ts
@@ -8,14 +8,15 @@ import { Info } from '../shared/registration';
 })
 export class ApplicationService {
   private url = 'https://academy-project-back.herokuapp.com/';
+  private proxyurl = 'https://cors-anywhere.herokuapp.com/';
   private readonly apiPath = '/api';
 
   constructor(private httpClient: HttpClient) { }
 
   getApplications(): Observable<Info[]> {
-    return this.httpClient.get<Info[]>(`${this.url}/${this.apiPath}/applications`);
+    return this.httpClient.get<Info[]>(`${this.proxyurl}${this.url}${this.apiPath}/applications`);
   }
   getApplication({ id }): Observable<Info> {
-    return this.httpClient.get<Info>(`${this.url}/${this.apiPath}/applications/${id}`);
+    return this.httpClient.get<Info>(`${this.proxyurl}${this.url}${this.apiPath}/applications/${id}`);
   }
 }
