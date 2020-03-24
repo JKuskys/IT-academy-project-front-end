74107ef491021f2b44c74d83759514988de2fe4f
JKuskys <48916581+JKuskys@users.noreply.github.com>
2020-03-24 14:24:33 +0200

    Fix/login (#41)
    * fix(while not logged in user gets redirected properly)
    
    * paraiškų mygtukas pridėtas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,8 @@ import {LoginComponent} from '../login/login.component';
 import {AuthGuardService} from '../Services/auth-guard.service';
 import {AuthServiceService} from '../Services/auth-service.service';
 import {Router} from '@angular/router';
+import {isNotNullOrUndefined} from 'codelyzer/util/isNotNullOrUndefined';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +14,7 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   private bodyText: string;
+  private jwtHelper: JwtHelperService;
 
   constructor(private dialog: MatDialog,
               private auth: AuthServiceService,
@@ -23,6 +26,37 @@ export class HeaderComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  adminCheck(): boolean {
+    const expectedRole = 'ADMIN';
+    // decode the token to get its payload
+    let token: string;
+    let tokenPayload: any;
+    // decode the token to get its payload
+    try {
+      token = localStorage.getItem('token');
+      tokenPayload = this.jwtHelper.decodeToken(token);
+    } catch (e) {
+      tokenPayload = null;
+    }
+    let roles: any;
+    if (isNotNullOrUndefined(tokenPayload)) {
+      roles = JSON.stringify(tokenPayload.roles);
+    } else {
+      roles = '';
+    }
+    console.log(roles);
+    if (
+      roles.includes(expectedRole) && this.logInCheck()
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  openApplications() {
+    this.router.navigate(['applications']).catch();
   }
 
   onLogOut() {
