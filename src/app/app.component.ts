5fadc24c126586d6f6c29359a4af0b557acb7260
jkuskys <j.kuskys1@gmail.com>
2020-03-30 14:44:20 +0300

    sessions are properly working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SessionService} from './services/authorization/session.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = "IT-academy-project-front-end";
+export class AppComponent implements OnInit {
+  title = 'IT-academy-project-front-end';
+
+  constructor(private sessionService: SessionService) {
+  }
+
+
+  ngOnInit() {
+    this.sessionService.onLogOut();
+  }
+
 }
